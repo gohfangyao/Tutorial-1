a31588c70a825e5ea57d07c3af905e808ffb57d2
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2019-09-20 15:12:28 +0800

    Version 0.1

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld{
+
+
+
+public static void main(String[] args) {
+
+
+    System.out.println("Hello World Malaysia STIA1113");
+
+    int A = 6*3*1/2;
+    int B = 4*3*1/2;
+    int C = 4*3;
+
+    System.out.println("A="+ A);
+    System.out.println("B="+ B);
+    System.out.println("C="+ C);
+    System.out.println("Total="+(A+B+C));
+  
+
+
+}
+
+}
