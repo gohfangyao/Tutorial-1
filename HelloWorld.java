f42c03558bccb1d6668f26ee0fc6e01a20a32b7f
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2019-09-20 14:56:43 +0800

    Version 0.1

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+
+
+
+public static void main(String[] args) {
+
+    System.out.println("C="+ 4*3);
+    System.out.println("A="+3*6*1/2);
+    System.out.println("B="+3*4*1/2);
+    System.out.println("A+B+C="+27);
+  
+
+
+}
+
+}
